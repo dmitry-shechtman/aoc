46b45d10d6de3e37e0e9682bf70f3a55948e3072
Dmitry Shechtman <damnian@gmail.com>
2023-12-02 11:15:07 +0200

    Add EnumerableExtensions

---
+++ aoc/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc
+{
+    public static class EnumerableExtensions
+    {
+        public static long Product(this IEnumerable<long> collection) =>
+            collection.Aggregate(1L, (a, v) => a * v);
+
+        public static long Product<T>(this IEnumerable<T> collection, Func<T, long> selector) =>
+            collection.Aggregate(1L, (a, v) => a * selector(v));
+    }
+}
