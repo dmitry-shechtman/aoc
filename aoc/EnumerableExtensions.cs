76d3e5916ccc86dbe3cf92611ae62de9a559484a
Dmitry Shechtman <damnian@gmail.com>
2023-12-03 10:15:23 +0200

    Add int Product()

--- aoc/EnumerableExtensions.cs
+++ aoc/EnumerableExtensions.cs
@@ -6,6 +6,12 @@ namespace aoc
 {
     public static class EnumerableExtensions
     {
+        public static int Product(this IEnumerable<int> collection) =>
+            collection.Aggregate(1, (a, v) => a * v);
+
+        public static int Product<T>(this IEnumerable<T> collection, Func<T, int> selector) =>
+            collection.Aggregate(1, (a, v) => a * selector(v));
+
         public static long Product(this IEnumerable<long> collection) =>
             collection.Aggregate(1L, (a, v) => a * v);
 
