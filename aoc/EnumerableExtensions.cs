f10f7c12d1a8d3fc73499a9d04de4872062d0b7c
Dmitry Shechtman <damnian@gmail.com>
2023-12-08 16:28:56 +0200

    Add Gcd() and Lcm()

--- aoc/EnumerableExtensions.cs
+++ aoc/EnumerableExtensions.cs
@@ -103,5 +103,53 @@ namespace aoc
 
         public static TResult TryMax<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector, TResult result) =>
             source.Any() ? source.Max(selector) : result;
+
+        public static int Gcd(this IEnumerable<int> source) =>
+            source.Aggregate(Gcd);
+
+        public static int Gcd<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector) =>
+            source.Select(selector).Aggregate(Gcd);
+
+        public static int Gcd<TSource>(this IEnumerable<TSource> source, Func<TSource, int, int> selector) =>
+            source.Select(selector).Aggregate(Gcd);
+
+        private static int Gcd(int a, int b) =>
+            b == 0 ? a : Gcd(b, a % b);
+
+        public static long Gcd(this IEnumerable<long> source) =>
+            source.Aggregate(Gcd);
+
+        public static long Gcd<TSource>(this IEnumerable<TSource> source, Func<TSource, long> selector) =>
+            source.Select(selector).Aggregate(Gcd);
+
+        public static long Gcd<TSource>(this IEnumerable<TSource> source, Func<TSource, int, long> selector) =>
+            source.Select(selector).Aggregate(Gcd);
+
+        private static long Gcd(long a, long b) =>
+            b == 0 ? a : Gcd(b, a % b);
+
+        public static int Lcm(this IEnumerable<int> source) =>
+            source.Aggregate(Lcm);
+
+        public static int Lcm<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector) =>
+            source.Select(selector).Aggregate(Lcm);
+
+        public static int Lcm<TSource>(this IEnumerable<TSource> source, Func<TSource, int, int> selector) =>
+            source.Select(selector).Aggregate(Lcm);
+
+        private static int Lcm(int a, int b) =>
+            a / Gcd(a, b) * b;
+
+        public static long Lcm(this IEnumerable<long> source) =>
+            source.Aggregate(Lcm);
+
+        public static long Lcm<TSource>(this IEnumerable<TSource> source, Func<TSource, long> selector) =>
+            source.Select(selector).Aggregate(Lcm);
+
+        public static long Lcm<TSource>(this IEnumerable<TSource> source, Func<TSource, int, long> selector) =>
+            source.Select(selector).Aggregate(Lcm);
+
+        private static long Lcm(long a, long b) =>
+            a / Gcd(a, b) * b;
     }
 }
