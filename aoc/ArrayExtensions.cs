6b8921c451d4c84544ee9d8328c2398130f0b43f
Dmitry Shechtman <damnian@gmail.com>
2023-12-08 14:21:23 +0200

    Add ArrayExtensions

---
+++ aoc/ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aoc
+{
+    public static class ArrayExtensions
+    {
+        public static int IndexOf<T>(this T[] array, T value) =>
+            Array.IndexOf(array, value);
+
+        public static int LastIndexOf<T>(this T[] array, T value) =>
+            Array.LastIndexOf(array, value);
+    }
+}
