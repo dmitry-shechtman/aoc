ec84a2bfe24bc2c5a228a196dd4dfdfa3b0cbd83
Dmitry Shechtman <damnian@gmail.com>
2023-12-15 19:36:13 +0200

    Add LinkedList and LinkedListNode

---
+++ aoc/LinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace aoc
+{
+    public sealed class LinkedList<T> : IEnumerable<T>
+    {
+        public LinkedListNode<T> First { get; private set; }
+        public LinkedListNode<T> Last  { get; private set; }
+
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            First = new(value, null, First);
+            if (First.Next is null)
+                Last = First;
+            return First;
+        }
+
+        public LinkedListNode<T> AddLast(T value)
+        {
+            Last = new(value, Last, null);
+            if (Last.Previous is null)
+                First = Last;
+            return Last;
+        }
+
+        public void Remove(LinkedListNode<T> node)
+        {
+            node.Remove();
+            if (node.Previous is null)
+                First = node.Next;
+            if (node.Next is null)
+                Last = node.Previous;
+        }
+
+        public LinkedListNode<T> Find(Func<T, bool> predicate)
+        {
+            for (var node = First; node is not null; node = node.Next)
+                if (predicate(node.Value))
+                    return node;
+            return null;
+        }
+
+        public LinkedListNode<T> FindLast(Func<T, bool> predicate)
+        {
+            for (var node = Last; node is not null; node = node.Previous)
+                if (predicate(node.Value))
+                    return node;
+            return null;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var node = First; node is not null; node = node.Next)
+                yield return node.Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+    }
+}
---
+++ aoc/LinkedListNode.cs
@@ -0,0 +1,33 @@
+﻿namespace aoc
+{
+    public sealed class LinkedListNode<T>
+    {
+        public LinkedListNode(T value, LinkedListNode<T> previous, LinkedListNode<T> next)
+        {
+            Value = value;
+            if ((Previous = previous) is not null)
+                Previous.Next = this;
+            if ((Next = next) is not null)
+                Next.Previous = this;
+        }
+
+        public LinkedListNode(T value)
+        {
+            Value = value;
+            Previous = this;
+            Next = this;
+        }
+
+        public T Value { get; set; }
+        public LinkedListNode<T> Previous { get; private set; }
+        public LinkedListNode<T> Next     { get; private set; }
+
+        public void Remove()
+        {
+            if (Previous is not null)
+                Previous.Next = Next;
+            if (Next is not null)
+                Next.Previous = Previous;
+        }
+    }
+}
