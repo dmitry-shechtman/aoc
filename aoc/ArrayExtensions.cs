f435e35624c5578bb5c26cdc9859d01840de2d0d
Dmitry Shechtman <damnian@gmail.com>
2023-12-16 19:18:03 +0200

    Add Clear()

--- aoc/ArrayExtensions.cs
+++ aoc/ArrayExtensions.cs
@@ -4,10 +4,16 @@ namespace aoc
 {
     public static class ArrayExtensions
     {
+        public static void Clear<T>(this T[] array) =>
+            Array.Clear(array, 0, array.Length);
+
         public static int IndexOf<T>(this T[] array, T value) =>
             Array.IndexOf(array, value);
 
         public static int LastIndexOf<T>(this T[] array, T value) =>
             Array.LastIndexOf(array, value);
+
+        public static void Sort<T>(this T[] array, Comparison<T> comparison) =>
+            Array.Sort(array, comparison);
     }
 }
