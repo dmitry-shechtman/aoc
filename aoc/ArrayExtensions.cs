68ed2a5f172aedb0f7050aa6f0eb283491d8f667
Dmitry Shechtman <damnian@gmail.com>
2023-12-18 13:40:42 +0200

    Add Find(), FindIndex(), FindLast() and FindLastIndex()

--- aoc/ArrayExtensions.cs
+++ aoc/ArrayExtensions.cs
@@ -7,6 +7,18 @@ namespace aoc
         public static void Clear(this Array array) =>
             Array.Clear(array, 0, array.Length);
 
+        public static T Find<T>(this T[] array, Predicate<T> match) =>
+            Array.Find(array, match);
+
+        public static int FindIndex<T>(this T[] array, Predicate<T> match) =>
+            Array.FindIndex(array, match);
+
+        public static T FindLast<T>(this T[] array, Predicate<T> match) =>
+            Array.FindLast(array, match);
+
+        public static int FindLastIndex<T>(this T[] array, Predicate<T> match) =>
+            Array.FindLastIndex(array, match);
+
         public static int IndexOf<T>(this T[] array, T value) =>
             Array.IndexOf(array, value);
 
