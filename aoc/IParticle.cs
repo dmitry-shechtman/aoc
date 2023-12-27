cb531636b851db2fd31149e0f76fe307a843df77
Dmitry Shechtman <damnian@gmail.com>
2023-12-27 20:03:19 +0200

    Implement IReadOnlyList in particle

--- aoc/IParticle.cs
+++ aoc/IParticle.cs
@@ -1,17 +1,42 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace aoc
 {
-    public interface IParticle<TSelf, TVector> : IEquatable<TSelf>
+    public interface IParticle<TSelf, TVector> : IEquatable<TSelf>, IReadOnlyList<TVector>
         where TSelf : struct, IParticle<TSelf, TVector>
         where TVector : struct
     {
+        private const int Cardinality = 3;
+
         TVector P { get; }
         TVector V { get; }
         TVector A { get; }
 
         void Deconstruct(out TVector p, out TVector v);
         void Deconstruct(out TVector p, out TVector v, out TVector a);
+
+        IEnumerator<TVector> IEnumerable<TVector>.GetEnumerator()
+        {
+            yield return P;
+            yield return V;
+            yield return A;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+
+        TVector IReadOnlyList<TVector>.this[int i] => i switch
+        {
+            0 => P,
+            1 => V,
+            2 => A,
+            _ => throw new IndexOutOfRangeException(),
+        };
+
+        int IReadOnlyCollection<TVector>.Count =>
+            Cardinality;
     }
 
     public interface IParticle<TSelf, TVector, T> : IParticle<TSelf, TVector>
