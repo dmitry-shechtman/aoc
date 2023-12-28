447c913faa4d02b8dcd23828a5a42cf8ae0e823f
Dmitry Shechtman <damnian@gmail.com>
2023-12-28 23:15:37 +0200

    Make IMatrix and IVector interdependent

--- aoc/IMatrix.cs
+++ aoc/IMatrix.cs
@@ -4,7 +4,7 @@ namespace aoc
 {
     public interface IMatrix<TSelf, TVector, T> : IEquatable<TSelf>
         where TSelf : struct, IMatrix<TSelf, TVector, T>
-        where TVector : struct, IVector<TVector, T>
+        where TVector : struct, IVector<TVector, TSelf, T>
         where T : struct
     {
         TVector Mul(TVector vector);
