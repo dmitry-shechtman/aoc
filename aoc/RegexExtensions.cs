99339d0fe5d28427a3ce29600c9e3454608fc7bc
Dmitry Shechtman <damnian@gmail.com>
2024-12-04 01:56:26 +0200

    Add RegexExtensions

---
+++ aoc/RegexExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System.Text.RegularExpressions
+{
+    public static class RegexExtensions
+    {
+#if !NET6_0_OR_GREATER
+        public static IEnumerable<TResult> Select<TResult>(this MatchCollection matches, Func<Match, TResult> selector) =>
+            Enumerable.Select(matches, selector);
+#endif
+
+#if !NET8_0_OR_GREATER
+        public static IEnumerable<TResult> Select<TResult>(this GroupCollection groups, Func<Group, TResult> selector) =>
+            Enumerable.Select(groups, selector);
+#endif
+    }
+}
