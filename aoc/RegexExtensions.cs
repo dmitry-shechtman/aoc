1b2d7866a25e097de35bd0653b3fe55bc24e3b77
Dmitry Shechtman <damnian@gmail.com>
2024-12-04 02:38:54 +0200

    Add ToDictionary(groups)

--- aoc/RegexExtensions.cs
+++ aoc/RegexExtensions.cs
@@ -13,6 +13,12 @@ namespace System.Text.RegularExpressions
 #if !NET8_0_OR_GREATER
         public static IEnumerable<TResult> Select<TResult>(this GroupCollection groups, Func<Group, TResult> selector) =>
             Enumerable.Select(groups, selector);
+
+        public static Dictionary<TKey, TElement> ToDictionary<TKey, TElement>(this GroupCollection groups,
+            Func<Group, TKey> keySelector,
+            Func<Group, TElement> elementSelector)
+            where TKey : notnull =>
+                Enumerable.ToDictionary(groups, keySelector, elementSelector);
 #endif
     }
 }
