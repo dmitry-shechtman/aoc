bc04b3afd5c10f6953b7c623ee9741aa6ca51469
Dmitry Shechtman <damnian@gmail.com>
2024-12-09 22:45:47 +0200

    Move VectorRangeHelper to separate file

--- aoc/Internal/RangeHelper.cs
+++ aoc/Internal/RangeHelper.cs
@@ -39,17 +39,4 @@ namespace aoc.Internal
         protected override RangeHelperStrategy<TRange, T> Strategy =>
             RangeHelperStrategy<TRange, T>.Instance;
     }
-
-    sealed class VectorRangeHelper<TRange, TVector> : Helper2<TRange, TVector, RangeHelperStrategy<TRange, TVector>, IVectorHelper<TVector>>
-        where TRange : struct, IRange<TRange, TVector>
-        where TVector : struct, IFormattable
-    {
-        public VectorRangeHelper(FromArray<TRange, TVector> fromArray, IVectorHelper<TVector> vector)
-            : base(fromArray, vector)
-        {
-        }
-
-        protected override RangeHelperStrategy<TRange, TVector> Strategy =>
-            RangeHelperStrategy<TRange, TVector>.Instance;
-    }
 }
---
+++ aoc/Internal/VectorRangeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace aoc.Internal
+{
+    sealed class VectorRangeHelper<TRange, TVector> : Helper2<TRange, TVector, RangeHelperStrategy<TRange, TVector>, IVectorHelper<TVector>>
+        where TRange : struct, IRange<TRange, TVector>
+        where TVector : struct, IFormattable
+    {
+        public VectorRangeHelper(FromArray<TRange, TVector> fromArray, IVectorHelper<TVector> vector)
+            : base(fromArray, vector)
+        {
+        }
+
+        protected override RangeHelperStrategy<TRange, TVector> Strategy =>
+            RangeHelperStrategy<TRange, TVector>.Instance;
+    }
+}
