2a1f9c8b8e30f6a97668f75ed0a7f5ce033e37da
Dmitry Shechtman <damnian@gmail.com>
2024-12-13 22:54:09 +0200

    Add argument checks

--- aoc/LinkedList.cs
+++ aoc/LinkedList.cs
@@ -202,28 +202,50 @@ namespace aoc
             return false;
         }
 
-        public static LinkedListNode<T> Find(LinkedListNode<T> node, Predicate<T> match)
+        public static LinkedListNode<T> Find(LinkedListNode<T> node, Predicate<T> match) =>
+            node is null
+                ? throw new ArgumentNullException(nameof(node))
+                : match is null
+                    ? throw new ArgumentNullException(nameof(match))
+                    : DoFind(node, match);
+
+        public LinkedListNode<T> Find(Predicate<T> match) =>
+            match is null
+                ? throw new ArgumentNullException(nameof(match))
+                : DoFind(First, match);
+
+        private static LinkedListNode<T> DoFind(LinkedListNode<T> node, Predicate<T> match)
         {
             while (node is not null && !match(node.Value))
                 node = node.Next;
             return node;
         }
 
-        public LinkedListNode<T> Find(Predicate<T> match) =>
-            Find(First, match);
+        public static LinkedListNode<T> FindPrevious(LinkedListNode<T> node, Predicate<T> match) =>
+            node is null
+                ? throw new ArgumentNullException(nameof(node))
+                : match is null
+                    ? throw new ArgumentNullException(nameof(match))
+                    : DoFindPrevious(node, match);
 
-        public static LinkedListNode<T> FindPrevious(LinkedListNode<T> node, Predicate<T> match)
+        public LinkedListNode<T> FindLast(Predicate<T> match) =>
+            match is null
+                ? throw new ArgumentNullException(nameof(match))
+                : DoFindPrevious(Last, match);
+
+        private static LinkedListNode<T> DoFindPrevious(LinkedListNode<T> node, Predicate<T> match)
         {
             while (node is not null && !match(node.Value))
                 node = node.Previous;
             return node;
         }
 
-        public LinkedListNode<T> FindLast(Predicate<T> match) =>
-            FindPrevious(Last, match);
-
         public void CopyTo(T[] array, int index)
         {
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+            if (index < 0 || index >= array.Length)
+                throw new ArgumentOutOfRangeException(nameof(index));
             for (var node = First; node is not null; node = node.Next)
                 array[index++] = node.Value;
         }
