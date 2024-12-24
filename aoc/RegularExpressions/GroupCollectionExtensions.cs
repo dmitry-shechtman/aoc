07dcfebbed7a32fea40ec1c2072c8b326d78cf98
Dmitry Shechtman <damnian@gmail.com>
2024-12-24 21:19:07 +0200

    Simplify Skip(groups, range)

--- aoc/RegularExpressions/GroupCollectionExtensions.cs
+++ aoc/RegularExpressions/GroupCollectionExtensions.cs
@@ -15,11 +15,11 @@ namespace System.Text.RegularExpressions
         public static IEnumerable<Group> Take(this GroupCollection groups, int count) =>
             Enumerable.Take<Group>(groups, count);
 
-        public static IEnumerable<Group> Skip(this GroupCollection groups, Range range)
+        public static IEnumerable<Group> Skip(this GroupCollection groups, Range? range)
         {
-            var start = GetIndex(range.Start, range, groups.Count);
-            var end = GetIndex(range.End, range, groups.Count, groups.Count);
-            return groups.Skip(start).Take(end - start);
+            var (offset, length) = range?.GetOffsetAndLength(groups.Count)
+                ?? (0, groups.Count);
+            return groups.Skip(offset).Take(length);
         }
 
         public static Dictionary<TKey, TElement> ToDictionary<TKey, TElement>(this GroupCollection groups,
@@ -27,49 +27,42 @@ namespace System.Text.RegularExpressions
             Func<Group, TElement> elementSelector)
             where TKey : notnull =>
                 Enumerable.ToDictionary(groups, keySelector, elementSelector);
-
-        private static int GetIndex(Index index, Range range, int count, int @default = 0) =>
-            index.IsFromEnd
-                ? count - index.Value
-                : range.Equals(default)
-                    ? @default
-                    : index.Value;
 #endif
 
-        public static T[] GetValues<T>(this GroupCollection groups, Range range = default)
+        public static T[] GetValues<T>(this GroupCollection groups, Range? range = null)
             where T : IConvertible =>
                 GetValues(groups, StringExtensions.ConvertTo<T>, range);
 
         public static T[] GetValues<T>(this GroupCollection groups,
-            Func<string, T> selector, Range range = default) =>
+            Func<string, T> selector, Range? range = null) =>
                 groups.Skip(range)
                     .Select(g => selector(g.Value)).ToArray();
 
-        public static IEnumerable<T> SelectValues<T>(this GroupCollection groups, Range range = default)
+        public static IEnumerable<T> SelectValues<T>(this GroupCollection groups, Range? range = null)
             where T : IConvertible =>
                 SelectValues(groups, StringExtensions.ConvertTo<T>, range);
 
         public static IEnumerable<T> SelectValues<T>(this GroupCollection groups,
-            Func<string, T> selector, Range range = default) =>
+            Func<string, T> selector, Range? range = null) =>
                 groups.Skip(range)
                     .Select(g => selector(g.Value));
 
-        public static Dictionary<string, T[]> GetAllValues<T>(this GroupCollection groups, Range range = default)
+        public static Dictionary<string, T[]> GetAllValues<T>(this GroupCollection groups, Range? range = null)
             where T : IConvertible =>
                 GetAllValues(groups, StringExtensions.ConvertTo<T>, range);
 
         public static Dictionary<string, T[]> GetAllValues<T>(this GroupCollection groups,
-            Func<string, T> selector, Range range = default) =>
+            Func<string, T> selector, Range? range = null) =>
                 groups.Skip(range)
                     .ToDictionary(g => g.Name, g => g.GetValues(selector));
 
         public static Dictionary<string, IEnumerable<T>> SelectAllValues<T>(this GroupCollection groups,
-            Range range = default)
+            Range? range = null)
                 where T : IConvertible =>
                     SelectAllValues(groups, StringExtensions.ConvertTo<T>, range);
 
         public static Dictionary<string, IEnumerable<T>> SelectAllValues<T>(this GroupCollection groups,
-            Func<string, T> selector, Range range = default) =>
+            Func<string, T> selector, Range? range = null) =>
                 groups.Skip(range)
                     .ToDictionary(g => g.Name, g => g.SelectValues(selector));
     }
--- aoc/RegularExpressions/RegexExtensions.cs
+++ aoc/RegularExpressions/RegexExtensions.cs
@@ -14,31 +14,31 @@ namespace System.Text.RegularExpressions
                 regex.Match(input).Groups.GetValues(selector);
 
         public static IEnumerable<T[]> SelectValuesMany<T>(this Regex regex, string input,
-            Range range = default)
+            Range? range = null)
                 where T : IConvertible =>
                     SelectValuesMany(regex, input, StringExtensions.ConvertTo<T>, range);
 
         public static IEnumerable<T[]> SelectValuesMany<T>(this Regex regex, string input,
-            Func<string, T> selector, Range range = default) =>
+            Func<string, T> selector, Range? range = null) =>
                 regex.Matches(input)
                     .Select(m => m.Groups.GetValues(selector, range));
 
         public static Dictionary<string, T[]> GetAllValues<T>(this Regex regex, string input,
-            Range range = default)
+            Range? range = null)
                 where T : IConvertible =>
                     GetAllValues(regex, input, StringExtensions.ConvertTo<T>, range);
 
         public static Dictionary<string, T[]> GetAllValues<T>(this Regex regex, string input,
-            Func<string, T> selector, Range range = default) =>
+            Func<string, T> selector, Range? range = null) =>
                 regex.Match(input).Groups.GetAllValues(selector, range);
 
         public static Dictionary<string, IEnumerable<T>> SelectAllValues<T>(this Regex regex, string input,
-            Range range = default)
+            Range? range = null)
                 where T : IConvertible =>
                     SelectAllValues(regex, input, StringExtensions.ConvertTo<T>, range);
 
         public static Dictionary<string, IEnumerable<T>> SelectAllValues<T>(this Regex regex, string input,
-            Func<string, T> selector, Range range = default) =>
+            Func<string, T> selector, Range? range = null) =>
                 regex.Match(input).Groups.SelectAllValues(selector, range);
     }
 }
