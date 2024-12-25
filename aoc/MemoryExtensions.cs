4cc163b6d01ceb2720d1b1a18078f0d11a1aa341
Dmitry Shechtman <damnian@gmail.com>
2024-12-25 23:50:44 +0200

    Add All(), Any(), Count(), Sum() and Product()

--- aoc/MemoryExtensions.cs
+++ aoc/MemoryExtensions.cs
@@ -5,6 +5,138 @@ namespace aoc
 {
     public static class MemoryExtensions
     {
+        public static bool All<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, bool> predicate)
+        {
+            for (int i = 0; i < source.Length; i++)
+                if (!predicate(source[i]))
+                    return false;
+            return true;
+        }
+
+        public static bool All<TSource>(this Span<TSource> source, Func<TSource, bool> predicate) =>
+            All((ReadOnlySpan<TSource>)source, predicate);
+
+        public static bool All<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            for (int i = 0; i < source.Length; i++)
+                if (!predicate(source[i], i))
+                    return false;
+            return true;
+        }
+
+        public static bool All<TSource>(this Span<TSource> source, Func<TSource, int, bool> predicate) =>
+            All((ReadOnlySpan<TSource>)source, predicate);
+
+        public static bool Any<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, bool> predicate)
+        {
+            for (int i = 0; i < source.Length; i++)
+                if (predicate(source[i]))
+                    return true;
+            return false;
+        }
+
+        public static bool Any<TSource>(this Span<TSource> source, Func<TSource, bool> predicate) =>
+            Any((ReadOnlySpan<TSource>)source, predicate);
+
+        public static bool Any<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            for (int i = 0; i < source.Length; i++)
+                if (predicate(source[i], i))
+                    return true;
+            return false;
+        }
+
+        public static bool Any<TSource>(this Span<TSource> source, Func<TSource, int, bool> predicate) =>
+            Any((ReadOnlySpan<TSource>)source, predicate);
+
+        public static int Count<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, bool> predicate)
+        {
+            int count = 0;
+            for (int i = 0; i < source.Length; i++)
+                count += predicate(source[i]) ? 1 : 0;
+            return count;
+        }
+
+        public static int Count<TSource>(this Span<TSource> source, Func<TSource, bool> predicate) =>
+            Count((ReadOnlySpan<TSource>)source, predicate);
+
+        public static int Count<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            int count = 0;
+            for (int i = 0; i < source.Length; i++)
+                count += predicate(source[i], i) ? 1 : 0;
+            return count;
+        }
+
+        public static int Count<TSource>(this Span<TSource> source, Func<TSource, int, bool> predicate) =>
+            Count((ReadOnlySpan<TSource>)source, predicate);
+
+        public static int Sum(this ReadOnlySpan<int> source)
+        {
+            int sum = 0;
+            for (int i = 0; i < source.Length; i++)
+                sum += source[i];
+            return sum;
+        }
+
+        public static int Sum(this Span<int> source) =>
+            Sum((ReadOnlySpan<int>)source);
+
+        public static int Sum<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, int> selector)
+        {
+            int sum = 0;
+            for (int i = 0; i < source.Length; i++)
+                sum += selector(source[i]);
+            return sum;
+        }
+
+        public static int Sum<TSource>(this Span<TSource> source, Func<TSource, int> selector) =>
+            Sum((ReadOnlySpan<TSource>)source, selector);
+
+        public static int Sum<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, int, int> selector)
+        {
+            int sum = 0;
+            for (int i = 0; i < source.Length; i++)
+                sum += selector(source[i], i);
+            return sum;
+        }
+
+        public static int Sum<TSource>(this Span<TSource> source, Func<TSource, int, int> selector) =>
+            Sum((ReadOnlySpan<TSource>)source, selector);
+
+        public static int Product(this ReadOnlySpan<int> source)
+        {
+            int product = 1;
+            for (int i = 0; i < source.Length; i++)
+                product *= source[i];
+            return product;
+        }
+
+        public static int Product(this Span<int> source) =>
+            Product((ReadOnlySpan<int>)source);
+
+        public static int Product<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, int> selector)
+        {
+            int product = 1;
+            for (int i = 0; i < source.Length; i++)
+                product *= selector(source[i]);
+            return product;
+        }
+
+        public static int Product<TSource>(this Span<TSource> source, Func<TSource, int> selector) =>
+            Product((ReadOnlySpan<TSource>)source, selector);
+
+        public static int Product<TSource>(this ReadOnlySpan<TSource> source, Func<TSource, int, int> selector)
+        {
+            int product = 1;
+            for (int i = 0; i < source.Length; i++)
+                product *= selector(source[i], i);
+            return product;
+        }
+
+        public static int Product<TSource>(this Span<TSource> source, Func<TSource, int, int> selector) =>
+            Product((ReadOnlySpan<TSource>)source, selector);
+
 #if !NET8_0_OR_GREATER
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Split(this ReadOnlySpan<char> source, Span<System.Range> destination, char separator, StringSplitOptions options = StringSplitOptions.None)
