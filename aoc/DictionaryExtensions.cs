912e62b87cdaca19e49646f4ad91c7b96b8f85f1
Dmitry Shechtman <damnian@gmail.com>
2024-12-27 06:00:37 +0200

    Add DictionaryExtensions

---
+++ aoc/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace aoc
+{
+    public static class DictionaryExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> vertices, TKey key, TValue value)
+        {
+            if (!vertices.TryGetValue(key, out var result))
+                vertices.Add(key, result = value);
+            return result;
+        }
+    }
+}
