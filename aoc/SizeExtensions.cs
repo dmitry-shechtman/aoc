504cccf93944172d04f903324a18f187f8e1dcec
Dmitry Shechtman <damnian@gmail.com>
2024-12-28 09:21:01 +0200

    Add SizeExtensions span overloads

--- aoc/SizeExtensions.cs
+++ aoc/SizeExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace aoc
+﻿using System;
+
+namespace aoc
 {
     public static class SizeExtensions
     {
@@ -7,10 +9,20 @@
             where T : struct =>
                 array[size.GetLongIndex(key)];
 
+        public static TValue GetValue<TSize, T, TValue>(this TSize size, ReadOnlySpan<TValue> span, T key)
+            where TSize : struct, IIntegerSize<TSize, T>
+            where T : struct =>
+                span[size.GetIndex(key)];
+
         public static TValue SetValue<TSize, T, TValue>(this TSize size, TValue[] array, T key, TValue value)
             where TSize : struct, IIntegerSize<TSize, T>
             where T : struct =>
                 array[size.GetLongIndex(key)] = value;
+
+        public static TValue SetValue<TSize, T, TValue>(this TSize size, Span<TValue> span, T key, TValue value)
+            where TSize : struct, IIntegerSize<TSize, T>
+            where T : struct =>
+                span[size.GetIndex(key)] = value;
 
         public static bool TryGetValue<TSize, T, TValue>(this TSize size, TValue[] array, T key, out TValue value)
             where TSize : struct, IIntegerSize<TSize, T>
@@ -22,6 +34,19 @@
                 return false;
             }
             value = GetValue(size, array, key);
+            return true;
+        }
+
+        public static bool TryGetValue<TSize, T, TValue>(this TSize size, ReadOnlySpan<TValue> span, T key, out TValue value)
+            where TSize : struct, IIntegerSize<TSize, T>
+            where T : struct
+        {
+            if (!size.Contains(key))
+            {
+                value = default;
+                return false;
+            }
+            value = GetValue(size, span, key);
             return true;
         }
     }
